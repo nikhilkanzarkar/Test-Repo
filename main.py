58f197c02c851513a5f2e12c43b708cea3284764
nikhilkanzarkar <nikhilkanzarkar@utexas.edu>
2018-08-07 19:59:15 -0500

    Last try

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
 print("Hello")
-print("Lets see if this works!")
-print("Git test #1")
-print("Git test #2")
-print("Git test #3")
-print("Final Test")
-print("Hello My name is... Jamesasdasdf")
-print("Finally")
